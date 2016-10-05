239b4002b56ed4cb90fced75f809c82d9d2e736a
Evin1- <edwin.hdz.12.0@gmail.com>
2016-10-05 11:00:35 -0500

    Remove junk code

--- app/src/main/java/com/example/rxjavasamples/MainActivity.java
+++ app/src/main/java/com/example/rxjavasamples/MainActivity.java
@@ -16,27 +16,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Authenticator.setDefault(new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("username", "password".toCharArray());
-            }
-        });
-
-        URL url = null;
-        try {
-            url = new URL("http://www.oracle.com");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        assert url != null;
-        try {
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
